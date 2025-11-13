029b4f49b72f84ee43d22318d66db334cbd0f5f1
IsaiBau <isaisabi02@gmail.com>
2025-11-13 17:10:35 -0600

    Prototipo de pedir permisos para la camara

--- app/src/main/java/com/edgardev/buystudy/ui/MainActivity.kt
+++ app/src/main/java/com/edgardev/buystudy/ui/MainActivity.kt
@@ -1,33 +1,100 @@
 package com.edgardev.buystudy.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.edgardev.buystudy.R
+import com.edgardev.buystudy.databinding.ActivityMainBinding
 import com.edgardev.buystudy.models.ProviderType
 import com.edgardev.buystudy.ui.components.BottomNavigation
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var layout: View
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MainScreen(
-                onLoginSuccess = { email, provider ->
-                    showHome(email, provider)
-                }
-            )
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        layout = binding.mainLayout
+        setContentView(view)
+
+
     }
 
-    private fun showHome(email: String, provider: ProviderType) {
-        val homeIntent = Intent(this, BottomNavigation::class.java).apply {
-            putExtra("email", email)
-            putExtra("provider", provider.name)
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Aceptado")
+            } else {
+                Log.i("Permission: ", "Denegado")
+            }
         }
-        startActivity(homeIntent)
-        finish()
-        Toast.makeText(this, "Successful Login", Toast.LENGTH_SHORT).show()
+
+    fun onClickRequestPermission(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_granted),
+                    Snackbar.LENGTH_INDEFINITE,
+                    null
+                ) {}
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+    }
+}
+fun View.showSnackbar(
+    view: View,
+    msg: String,
+    length: Int,
+    actionMessage: CharSequence?,
+    action: (View) -> Unit
+) {
+    val snackbar = Snackbar.make(view, msg, length)
+    if (actionMessage != null) {
+        snackbar.setAction(actionMessage) {
+            action(this)
+        }.show()
+    } else {
+        snackbar.show()
     }
 }
